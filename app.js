0704c3d659207a2765a43773ce9e3c7646dc34ad
ravenjoswell <rjc.panlaqui@gmail.com>
2024-06-22 07:22:58 -1000

    functionality completed

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+let guess_count = 0
+let answer = Math.floor(Math.random() * 101)
+let submit = document.querySelector('button')
+let correct_answer = document.getElementById('correct')
+let guesses = document.getElementById("guesses")
+
+console.log(answer)
+
+const checker = () => {
+    let input = document.querySelector('input').value
+    guess_count++
+
+    if (parseInt(input) === answer) {
+        correct_answer.style.display = 'block'
+    } else if(parseInt(input) > answer){
+        let new_guess = document.createElement('div')
+        new_guess.innerText = `Too high! That was guess: ${guess_count}. You guessed: ${input}`
+        guesses.appendChild(new_guess)
+       
+    } else if(parseInt(input) < answer){
+        let new_guess = document.createElement('div')
+        new_guess.innerText = `Too low! That was guess: ${guess_count}. You guessed: ${input}`
+        guesses.appendChild(new_guess)
+    }
+}
+
+submit.addEventListener('click', checker);
+
+
+
+
+
+
